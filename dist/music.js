664fb45bfe89628f497ab60b774446c0e3342461
yake <563168458@qq.com>
2019-06-07 00:28:15 +0800

    Site updated: 2019-06-07 00:28:13

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    lrcType: 3,
+    audio: [
+        {
+            name: "在水一方",
+            artist: '李健',
+            url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n2/31/23/2648161877.mp3',
+            cover: 'http://star.kuwo.cn/star/starheads/180/73/49/1131928591.jpg',
+            lrc: 'song-post/zaishuiyifang.lrc'
+        },
+    ]
+});
